a26c3461548eb7b270a12efd2b3dfadff3b501c9
Fang MinJie <minjie_fang@odao.com>
2020-04-15 15:05:21 +0800

    Add first ruby code

---
+++ lib/bson/document.rb
@@ -0,0 +1,48 @@
+module BSON
+
+  # Injects behaviour for BSON::Document.
+  #
+  # @since 1.0.0
+  class Document
+
+    # Retrieves the value object corresponding to the each key objects repeatedly.
+    # Will normalize symbol keys into strings.
+    # Will returns &block.call if the returned value is nil
+    #
+    # @since 1.0.0
+    def dig_or_get(*args, &block)
+      value = self.dig *args
+      if value.nil? && !block.nil?
+        value = block.call
+      end
+      value
+    end
+
+    # Get embedded value.
+    #
+    # @since 1.0.0
+    def get_embedded(path, *args, &block)
+      keys = path.split '.'
+      value = self
+      keys.each do |key|
+        if value.is_a? BSON::Document
+          value = value[key]
+        elsif value.is_a? Array
+          if /^\d+$/.match key
+            value = value[key.to_i]
+          else
+            return block.nil? ? (args.empty? ? nil : args.first) : block.call
+          end
+        else
+          return block.nil? ? (args.empty? ? nil : args.first) : block.call
+        end
+      end
+      if value.nil?
+        return block.nil? ? (args.empty? ? nil : args.first) : block.call
+      end
+      return value
+    end
+
+  end
+
+end
